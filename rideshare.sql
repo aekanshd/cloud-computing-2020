d8a355701fe48af7b911740edfbd39b558e45312
Varun Gupta <varungupta3009@gmail.com>
2020-01-30 22:03:44 +0530

    Add data to the DB

--- rideshare.sql
+++ rideshare.sql
@@ -7,10 +7,13 @@
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
+SET SQL_MODE
+= "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT
+= 0;
 START TRANSACTION;
-SET time_zone = "+00:00";
+SET time_zone
+= "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -28,16 +31,22 @@ SET time_zone = "+00:00";
 -- Table structure for table `locations`
 --
 
-CREATE TABLE `locations` (
-  `locationid` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL
+CREATE TABLE `locations`
+(
+  `locationid` int
+(11) NOT NULL,
+  `name` varchar
+(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `locations`
 --
 
-INSERT INTO `locations` (`locationid`, `name`) VALUES
+INSERT INTO `locations` (`
+locationid`,
+`name
+`) VALUES
 (1, 'Kempegowda Ward'),
 (2, 'Chowdeswari Ward'),
 (3, 'Atturu'),
@@ -243,18 +252,32 @@ INSERT INTO `locations` (`locationid`, `name`) VALUES
 -- Table structure for table `rides`
 --
 
-CREATE TABLE `rides` (
-  `rideid` int(11) NOT NULL,
-  `ownerid` int(11) NOT NULL,
-  `source` int(11) NOT NULL,
-  `destination` int(11) NOT NULL,
-  `time` varchar(20) NOT NULL
+CREATE TABLE `rides`
+(
+  `rideid` int
+(11) NOT NULL,
+  `ownerid` int
+(11) NOT NULL,
+  `source` int
+(11) NOT NULL,
+  `destination` int
+(11) NOT NULL,
+  `time` varchar
+(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `rides`
 --
 
+INSERT INTO `rides` (`
+rideid`,
+`ownerid
+`, `source`, `destination`, `time`) VALUES
+(1, 1, 3, 114, '2020-01-26 13:27:16'),
+(2, 3, 3, 114, '2020-01-26 13:28:20'),
+(3, 2, 3, 192, '2020-01-26 13:28:45'),
+(4, 1, 1, 3, '2015-03-25 06:30:00');
 
 -- --------------------------------------------------------
 
@@ -262,11 +285,25 @@ CREATE TABLE `rides` (
 -- Table structure for table `transactions`
 --
 
-CREATE TABLE `transactions` (
-  `rideid` int(11) NOT NULL,
-  `userid` int(11) NOT NULL,
-  `time` varchar(20) NOT NULL
+CREATE TABLE `transactions`
+(
+  `rideid` int
+(11) NOT NULL,
+  `userid` int
+(11) NOT NULL,
+  `time` varchar
+(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `transactions` (`
+rideid`,
+`userid
+`, `time`) VALUES
+(1, 1, '2020-01-30 15:01:06'),
+(1, 2, '2020-01-30 15:01:06'),
+(1, 3, '2020-01-30 15:01:06'),
+(2, 1, '2020-01-30 15:01:06'),
+(3, 3, '2020-01-30 15:01:06');
 
 -- --------------------------------------------------------
 
@@ -274,9 +311,12 @@ CREATE TABLE `transactions` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE `users` (
-  `userid` int(11) NOT NULL,
-  `username` varchar(50) NOT NULL,
+CREATE TABLE `users`
+(
+  `userid` int
+(11) NOT NULL,
+  `username` varchar
+(50) NOT NULL,
   `password` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -291,29 +331,37 @@ CREATE TABLE `users` (
 -- Indexes for table `locations`
 --
 ALTER TABLE `locations`
-  ADD PRIMARY KEY (`locationid`);
+ADD PRIMARY KEY
+(`locationid`);
 
 --
 -- Indexes for table `rides`
 --
 ALTER TABLE `rides`
-  ADD PRIMARY KEY (`rideid`),
-  ADD KEY `rides_ibfk_1` (`ownerid`),
-  ADD KEY `source` (`source`),
-  ADD KEY `destination` (`destination`);
+ADD PRIMARY KEY
+(`rideid`),
+ADD KEY `rides_ibfk_1`
+(`ownerid`),
+ADD KEY `source`
+(`source`),
+ADD KEY `destination`
+(`destination`);
 
 --
 -- Indexes for table `transactions`
 --
 ALTER TABLE `transactions`
-  ADD UNIQUE KEY `rideid` (`rideid`,`userid`),
-  ADD KEY `userid` (`userid`);
+ADD UNIQUE KEY `rideid`
+(`rideid`,`userid`),
+ADD KEY `userid`
+(`userid`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`userid`);
+ADD PRIMARY KEY
+(`userid`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -323,13 +371,15 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `rides`
 --
 ALTER TABLE `rides`
-  MODIFY `rideid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `rideid` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `userid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `userid` int
+(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
 
 --
 -- Constraints for dumped tables
@@ -339,17 +389,29 @@ ALTER TABLE `users`
 -- Constraints for table `rides`
 --
 ALTER TABLE `rides`
-  ADD CONSTRAINT `rides_ibfk_1` FOREIGN KEY (`ownerid`) REFERENCES `users` (`userid`),
-  ADD CONSTRAINT `rides_ibfk_2` FOREIGN KEY (`source`) REFERENCES `locations` (`locationid`),
-  ADD CONSTRAINT `rides_ibfk_3` FOREIGN KEY (`destination`) REFERENCES `locations` (`locationid`);
+ADD CONSTRAINT `rides_ibfk_1` FOREIGN KEY
+(`ownerid`) REFERENCES `users`
+(`userid`),
+ADD CONSTRAINT `rides_ibfk_2` FOREIGN KEY
+(`source`) REFERENCES `locations`
+(`locationid`),
+ADD CONSTRAINT `rides_ibfk_3` FOREIGN KEY
+(`destination`) REFERENCES `locations`
+(`locationid`);
 
 --
 -- Constraints for table `transactions`
 --
 ALTER TABLE `transactions`
-  ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
-  ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY (`userid`) REFERENCES `users` (`userid`),
-  ADD CONSTRAINT `transactions_ibfk_3` FOREIGN KEY (`rideid`) REFERENCES `rides` (`rideid`);
+ADD CONSTRAINT `transactions_ibfk_1` FOREIGN KEY
+(`userid`) REFERENCES `users`
+(`userid`),
+ADD CONSTRAINT `transactions_ibfk_2` FOREIGN KEY
+(`userid`) REFERENCES `users`
+(`userid`),
+ADD CONSTRAINT `transactions_ibfk_3` FOREIGN KEY
+(`rideid`) REFERENCES `rides`
+(`rideid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
