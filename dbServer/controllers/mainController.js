b1cddd8e765c41cbd0917eb3cca0066cc2240345
RAVENDRA SINGH <ravendras2@gmail.com>
2020-04-09 17:20:20 +0530

    new commit

--- dbServer/controllers/mainController.js
+++ dbServer/controllers/mainController.js
@@ -4,84 +4,94 @@ const fs = require('fs')
 const sql = require('../models/db.js')
 const request = require('request-promise')
 let query = ''
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if (error0) {
+    throw error0;
+  }
+  connection.createChannel(function(error1, channel) {
+    if (error1) {
+      throw error1;
+    }
+    var queue = 'slave';
 
-rabbitServer = 'amqp://localhost'
-exports.home = (req, res, next) => {
-	res.send("Hello Team 2020!")
-}
-exports.sendtoMaster = (req, res, next)=>{
+    channel.assertQueue(queue, {
+      durable: false});
+    console.log(" [*] Waiting for messages in %s. To exit press CTRL+C", queue);
 
-	amqp.connect(rabbitServer, function(error0, connection) {
-  		if (error0) {
-    		throw error0;
-  		}
-  	connection.createChannel(function(error1, channel) {
-  		if(error1){
-  			throw error1;
-  		}
-  		var queue = 'master';
+    channel.consume(queue, function(msg) {
+        console.log(" [x] Received %s", msg.content.toString());
+        console.log("Reading Database..")
+		mongoClient.connect(url, function(err, db) {
+			if(err){
+					console.error(err.message)
+
+					amqp.connect('amqp://localhost', function(error0, connection) {
+  					if (error0) {
+    					throw error0;
+  					}
+  					connection.createChannel(function(error1, channel) {
+  					if(error1){
+  						throw error1;
+  					}
+  					var queue = 'response';
   		
-  		channel.assertQueue(queue, {
-  			durable: false
-  		});
-  		channel.sendToQueue(queue, Buffer.from(req));
+  					channel.assertQueue(queue, {
+  						durable: false
+  					});
+  				channel.sendToQueue(queue, Buffer.from(req));
 
-  	});
-    setTimeout(function() {
-       connection.close();
-       process.exit(0);
-   }, 500);
-});
+  				});
+    			setTimeout(function() {
+     			  connection.close();
+     			  process.exit(0);
+   				}, 500);
+			}
+			dbo=db.db(dbConfig.DB)
+			//var qry = req.body.where;
+			console.log(req.body.where);
+			var qry = req.body.where;
+			if(req.body.where._id){
+				qry = {"_id":new objectId(req.body.where._id)}
+			}
+			dbo.collection(req.body.table).find(qry).toArray(function(err, db_out) {
+				if(err){
+					console.error(err.message)
+					return res.status(400).send(err)
+				}
+				console.log("Read Successful...\n",db_out);
+				db.close();
 
-exports.sendtoSlave = (req, res, next)=>{
 
-	amqp.connect(rabbitServer, function(error0, connection) {
-  		if (error0) {
-    		throw error0;
-  		}
-  	connection.createChannel(function(error1, channel) {
-  		if(error1){
-  			throw error1;
-  		}
-  		var queue = 'slave';
+
+				amqp.connect('amqp://localhost', function(error0, connection) {
+  				if (error0) {
+    				throw error0;
+  				}
+  				connection.createChannel(function(error1, channel) {
+  				if(error1){
+  					throw error1;
+  				}
+  				var queue = 'response';
   		
-  		channel.assertQueue(queue, {
-  			durable: false
-  		});
-  		channel.sendToQueue(queue, Buffer.from(req));
+  				channel.assertQueue(queue, {
+  					durable: false
+  				});
+  				channel.sendToQueue(queue, Buffer.from(req));
 
-  	});
-    setTimeout(function() {
-       connection.close();
-       process.exit(0);
-   }, 500);
+  				});
+    			setTimeout(function() {
+     			  connection.close();
+     			  process.exit(0);
+   				}, 500);
+				return res.status(200).send(db_out);
+			});
+		});
+
+
+
+
+       	}, {
+            noAck: true
+        });
+  });
 });
-
-
-
-// 8. Write data to the DB
-
-exports.writeDb = (req, res, next) => {
-	console.log("DB api");
-	if (req.method === "POST") {
-		console.log("Recieved DB write POST request..");
-		sendtoMaster(req, res, next);
-		}
-
-		else {
-			res.status(400).send("Table Not Supported")
-		}
-	}
-
-
-// 9. Read data from the DB
-
-exports.readDb = (req, res, next) => {
-	console.log("Reading Database..")
-	sendtoSlave(req, res, next);
-}
-
-// 10. Clear DB
- exports.clearDb = (req, res, next) => {
-	sendtoMaster(req, res, next);
-}
