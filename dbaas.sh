3684df4ac29bb86e3f142bb1719953598e65fbcb
RAVENDRA SINGH <ravendras2@gmail.com>
2020-04-12 15:33:22 +0530

    new commit

--- dbaas.sh
+++ dbaas.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-docker-compoose up -f rabbitmq/docker-compose.yml -d
-docker-compoose up -f orchestrator/docker-compose.yml -d
+docker-compose up -f rabbitmq/docker-compose.yml -d
+docker-compose up -f orchestrator/docker-compose.yml -d
 docker network connect rabbitmq_network orchestrator_network
