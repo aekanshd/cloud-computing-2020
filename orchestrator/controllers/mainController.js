c9c0c7bfd0f9fa59f84eca4e0eca4e0f75952763
Hrishikesh V <38471846+HrishikeshVish@users.noreply.github.com>
2020-04-28 14:44:20 +0530

    Created AddContainer and deleteContainer API

--- orchestrator/controllers/mainController.js
+++ orchestrator/controllers/mainController.js
@@ -2,8 +2,12 @@ var amqp = require('amqplib/callback_api');
 const path = require('path')
 const fs = require('fs')
 const Docker = require('dockerode');
-
+var build = require('dockerode-build')
+var pump = require('pump');
 const request = require('request-promise')
+const port = 27017;
+
+
 
 
 rabbit = {
@@ -20,6 +24,78 @@ const opt = {
 
 exports.home = (req, res, next) => {
 	res.send("Hello Team 2020!")
+}
+
+var createOptions = {
+	Image:"newimage",
+    Tty:true,
+    ExposedPorts: {
+        "3000/tcp:": {},
+        "1883/tcp:": {}
+    },
+    Env:[
+        "DBHOST=" + dbHost,
+    ],
+    Cmd:[
+        "/bin/bash", "-c", "nodejs /opt/ifi/lib/ifi/app.js"
+    ],
+    HostConfig:{
+        Binds: ["/Users/cefn/Documents/code/imagination/git/ifi/impl/releases/keynote/0.1.0:/opt/ifi"],
+    }
+};
+
+var startOptions = {
+    PortBindings: {
+        "3000/tcp": [{
+            "HostIP":"0.0.0.0",
+            "HostPort": "3000"
+        }],
+        "1883/tcp": [{
+            "HostIP":"0.0.0.0",
+            "HostPort": "1883"
+        }],
+    },
+};
+
+
+exports.createContainer = (req, res, next) =>{
+	port = port + 1;
+	let container_id;
+	let docker = new Dockerode({
+		host:info.Driver.IPAddress,
+		port: port
+	});
+	docker.buildImage('/DbServer/Dockerfile', {t:'newimage'},
+	,function(err, stream){
+		if(err) return res.status(500).send(err);
+		stream.pipe(process.stdout, {
+			end: true;
+		});
+		stream.on('end', function(){
+			docker.createContainer(createOptions, function(err, result){
+				if(err) return res.status(500).send(err);
+				else resolve(docker.getContainer(result.id));
+				container_id = result.id;
+			});
+			var container = docker.getContainer(container_id);
+			container.start(startOptions, function(err, data){
+				if(err) return res.status(500).send(err);
+				console.log(data);
+			});
+		});
+	});
+}
+exports.deleteContainer = (req, res, next) =>{
+	container_id = req.containerId;
+	let docker = new Docker();
+	var container = docker.getContainer(container_id);
+	
+	container.remove(function(err, data){
+		if(err) return res.status(500).send(err);
+		else console.log(data);
+		port = port - 1;
+	});
+
 }
 
 exports.writeDb = (req, res, next) => {
