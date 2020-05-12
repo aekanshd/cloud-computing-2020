5da4b88cf4023a2a7c5f090933441d7335921531
Ravendra Singh <ravendras2@gmail.com>
2020-05-13 00:20:11 +0530

    fixing errors

--- dbaas.sh
+++ dbaas.sh
@@ -4,6 +4,6 @@ docker-compose -f rabbitmq/docker-compose.yml up --build -d
 docker-compose -f zookeeper/docker-compose.yml up --build -d
 docker-compose -f dbServer/docker-compose-master.yml up --build -d
 docker-compose -f dbServer/docker-compose-slave.yml up --build -d
-docker-compose -f orchestrator/docker-compose.yml up --build -d
+docker-compose -f orchestrator/docker-compose.yml up --build
 
 
