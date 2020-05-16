53c7f0b660b790ef584eff2cfef9c359bebec06f
Ubuntu <ubuntu@ip-172-31-92-15.ec2.internal>
2020-05-16 16:56:30 +0000

    fsvsc

--- dbaas.sh
+++ dbaas.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-docker-compose -f rabbitmq/docker-compose.yml up --build -d
-docker-compose -f zookeeper/docker-compose.yml up --build -d
-docker-compose -f dbServer/docker-compose-master.yml up --build -d
-docker-compose -f dbServer/docker-compose-slave.yml up --build -d
-docker-compose -f orchestrator/docker-compose.yml up --build
+docker-compose -f rabbitmq/docker-compose.yml up --build --force-recreate -d
+docker-compose -f zookeeper/docker-compose.yml up --build --force-recreate -d
+docker-compose -f dbServer/docker-compose-master.yml up --force-recreate --build -d
+docker-compose -f dbServer/docker-compose-slave.yml up --force-recreate --build -d
+docker-compose -f orchestrator/docker-compose.yml up --force-recreate --build
 
 
